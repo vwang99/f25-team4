9205083467655dad7e9cfd8e23e27fd61a7cd3f6
JesusAJLopez <jesusjutlopezftw@gmail.com>
2025-10-28 12:06:02 -0400

    Changed chatroom java class to add creator field which links to user. and added members and reviews relationships.

---
+++ f25-team4-jlopez-milestone5/backend-api/src/main/java/com/f25_team4/tether/TetherApplication.java
@@ -0,0 +1,13 @@
+package com.f25_team4.tether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetherApplication.class, args);
+	}
+
+}
---
+++ f25-team4-jlopez-milestone5/backend-api/src/main/java/com/f25_team4/tether/user/AppUserController.java
@@ -0,0 +1,52 @@
+package com.f25_team4.tether.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class AppUserController {
+
+    @Autowired
+    private AppUserService appUserService;
+
+    @GetMapping
+    public List<AppUser> getAllUsers() {
+        return appUserService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public AppUser getUser(@PathVariable Long id) {
+        return appUserService.getUserById(id).orElseThrow();
+    }
+
+    @PostMapping
+    public AppUser createUser(@RequestBody AppUser user) {
+        return appUserService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public AppUser updateUser(@PathVariable Long id, @RequestBody AppUser user) {
+        return appUserService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        appUserService.deleteUser(id);
+    }
+
+    @PatchMapping("/{id}/subscribe")
+    public AppUser toggleSubscription(@PathVariable Long id) {
+       AppUser user = appUserService.getUserById(id).orElseThrow();
+
+       boolean currentStatus = user.isSubscribed();
+       user.setSubscribed(!currentStatus);
+
+       AppUser updatedUser = appUserService.saveUser(user);
+
+       return updatedUser;
+    }
+
+}
